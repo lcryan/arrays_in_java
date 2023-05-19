b06e7853f5dae31664fdd7b973d502553a624a2d
LuciaRyan <lcryan06@gmail.com>
2023-05-19 15:51:46 +0200

    first part done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        String[] countdown  = {"one","two","three"};
+        String[] countdown = {"one", "two", "three"};
         String[] names = {"Sjaak", "Sjimmy", "Sjonny"};
 
-        // implementeer de countdown met een array three...two...one... boom!
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        // 1. implementeer de countdown met een array three...two...one... boom!
+        System.out.println(countdown[0]);
+        System.out.println(countdown[1]);
+        System.out.println(countdown[2]);
         System.out.println("boom!");
 
-        // implementeer een for loop die alle namen in de array "names" print. Houd er rekening mee dat de array een andere length kan hebben dan 3.
+
+        // 2. implementeer een for loop die alle namen in de array "names" print. Houd er rekening mee dat de array een andere length kan hebben dan 3.
         //for(;;) {
 
         //}
 
-        // implementeer een for loop die de twee arrays combineerd. De output moet zijn:
+        // 3. implementeer een for loop die de twee arrays combineerd. De output moet zijn:
         // oneSjaak
         // twoSjimmy
         // threeSjonny

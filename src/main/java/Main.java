863ed19f71ec8605c381d00840165bb544df97f5
LuciaRyan <lcryan06@gmail.com>
2023-05-19 16:37:00 +0200

    third part done - assignment solved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,25 @@
+
+
 public class Main {
 
     public static void main(String[] args) {
         String[] countdown = {"one", "two", "three"};
         String[] names = {"Sjaak", "Sjimmy", "Sjonny"};
 
-        // 1. implementeer de countdown met een array three...two...one... boom!
         System.out.println(countdown[0]);
         System.out.println(countdown[1]);
         System.out.println(countdown[2]);
         System.out.println("boom!");
 
-        for (String str : names) {
-            System.out.println(str);
+        int length = countdown.length;
+        String[] result = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            result[i] = countdown[i] + names[i];
+            System.out.println(result[i]);
+
         }
 
-
-        // 3. implementeer een for loop die de twee arrays combineerd. De output moet zijn:
-        // oneSjaak
-        // twoSjimmy
-        // threeSjonny
-
-        //for(;;) {
-
-        //}
     }
 
 }
